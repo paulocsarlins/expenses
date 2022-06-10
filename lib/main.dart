7d5c6d37e5dd3f0ae5bdfafe80fd8a0457b8be9e
Paulo César Lins <paulocsar91@gmail.com>
2022-06-10 17:32:30 -0300

    Usando TextField #01

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,6 @@ class MyHomePage extends StatelessWidget {
           title: Text('Despesas Pessoais'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
@@ -91,6 +90,36 @@ class MyHomePage extends StatelessWidget {
                   ),
                 );
               }).toList(),
+            ),
+            Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Valor (R\$)',
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        FlatButton(
+                          onPressed: () {}, 
+                          child: Text('Nova Transação'),
+                          textColor: Colors.purple,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             )
           ],
         ));
