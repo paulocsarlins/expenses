afe41c8b251cd09534a4abc81b993c8516936593
Paulo César Lins <paulocsar91@gmail.com>
2022-06-11 17:53:24 -0300

    Teclado Numérico

--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -22,37 +22,7 @@ class _TransactionUserState extends State<TransactionUser> {
       title: 'Conta #02',
       value: 211.30,
       date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't3',
-      title: 'Conta #03',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't4',
-      title: 'Conta #04',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't5',
-      title: 'Conta #05',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't6',
-      title: 'Conta #06',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't7',
-      title: 'Conta #07',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
+    )
   ];
 
   _addTransaction(String title, double value) {
