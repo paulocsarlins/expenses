816bf484ddfcabea882f5ba1061ece2d068494aa
Paulo César Lins <paulocsar91@gmail.com>
2022-06-11 22:07:41 -0300

    Fechando Formulário (Modal)

--- lib/main.dart
+++ lib/main.dart
@@ -19,49 +19,49 @@ class ExpensesApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
-  
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>{
+class _MyHomePageState extends State<MyHomePage> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Conta #01',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta #02',
+      value: 211.30,
+      date: DateTime.now(),
+    )
+  ];
 
-final _transactions = [
-  Transaction(
-    id: 't1',
-    title: 'Conta #01',
-    value: 310.76,
-    date: DateTime.now(),
-  ),
-  Transaction(
-    id: 't2',
-    title: 'Conta #02',
-    value: 211.30,
-    date: DateTime.now(),
-  )
-];
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
 
-_addTransaction(String title, double value) {
-  final newTransaction = Transaction(
-    id: Random().nextDouble().toString(),
-    title: title,
-    value: value,
-    date: DateTime.now(),
-  );
+    setState(() {
+      _transactions.add(newTransaction);
+    });
 
-  setState(() {
-    _transactions.add(newTransaction);
-  });
-}
+    Navigator.of(context).pop(); // Fecha o modal
+  }
 
-_openTransactionOpenFormModal(BuildContext context) {
-  showModalBottomSheet(
-    context: context,
-    builder: (_) {
-        return TransactionForm(_addTransaction);
-    }
-  );
-}
+  _openTransactionOpenFormModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return TransactionForm(_addTransaction);
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
