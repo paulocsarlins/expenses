ac68a1d594e804f35265f3b18a8f1829849e31f7
Paulo César Lins <paulocsar91@gmail.com>
2022-06-25 00:11:49 -0300

    Usando Chart Widget

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -1,9 +1,54 @@
 import 'package:flutter/material.dart';
 
-class ChatBar extends StatelessWidget {
+class ChartBar extends StatelessWidget {
+  final String? label;
+  final double? value;
+  final double? percentage;
+
+  const ChartBar({
+    this.label,
+    this.value,
+    this.percentage,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Text('R\$${value!.toStringAsFixed(2)}'),
+        const SizedBox(height: 5),
+        SizedBox(
+          height: 60,
+          width: 10,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  color: const Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(5)
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 5),
+        Text(label!),
+      ],
+    );
   }
 }
