4607137b5246b05f97d4d865e5d881209320a6e6
Paulo César Lins <paulocsar91@gmail.com>
2022-10-14 18:47:42 -0300

    Orientações suportadas.

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,8 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    /*  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]); */
+
     return MaterialApp(
       home: const MyHomePage(),
       theme: tema.copyWith(
@@ -54,6 +56,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactions = [];
+  bool _showChart = false;
 
   List<Transaction> get _recentTransactions {
     return _transactions.where((tr) {
@@ -104,20 +107,37 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ],
     );
-    final availableHeight =
-        MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top;
+    final availableHeight = MediaQuery.of(context).size.height -
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
     return Scaffold(
       appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-                height: availableHeight * 0.25,
-                child: Chart(_recentTransactions)),
-            Container(
-                height: availableHeight * 0.75,
-                child: TransactionList(_transactions, _removeTransaction)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Exibir Gráfico'),
+                Switch(
+                  value: _showChart,
+                  onChanged: (value) {
+                    setState(() {
+                      _showChart = value;
+                    });
+                  },
+                ),
+              ],
+            ),
+            if (_showChart)
+              Container(
+                  height: availableHeight * 0.25,
+                  child: Chart(_recentTransactions)),
+            if (!_showChart)
+              Container(
+                  height: availableHeight * 0.75,
+                  child: TransactionList(_transactions, _removeTransaction)),
           ],
         ),
       ),
