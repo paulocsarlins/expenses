998a9d182fffee9f6fb1ab11d5a1e2b6497c0953
Paulo César Lins <paulocsar91@gmail.com>
2022-10-15 21:45:54 -0300

    Botão Adaptativo.

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'adaptative_button.dart';
 
 class TransactionForm extends StatefulWidget {
   final void Function(String, double, DateTime) onSubmit;
@@ -99,14 +100,9 @@ class _TransactionFormState extends State<TransactionForm> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
-                  ElevatedButton(
-                    onPressed: _submitForm,
-                    child: Text(
-                      'Nova Transação',
-                      style: TextStyle(
-                        color: Theme.of(context).textTheme.button?.color,
-                      ),
-                    ),
+                  AdaptativeButton(
+                    'Nova Transação', 
+                    _submitForm,
                   ),
                 ],
               ),
