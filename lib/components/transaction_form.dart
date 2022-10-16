9671da49f02bb8a41577fd61733437fab916bc14
Paulo César Lins <paulocsar91@gmail.com>
2022-10-15 22:37:28 -0300

    DatePicker Adaptativo.

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'adaptative_button.dart';
+import 'adaptative_date_picker.dart';
 import 'adaptative_text_field.dart';
 
 class TransactionForm extends StatefulWidget {
@@ -28,23 +28,6 @@ class _TransactionFormState extends State<TransactionForm> {
     widget.onSubmit(title, value, _selectedDate!);
   }
 
-  _showDatePicker() {
-    showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2019),
-      lastDate: DateTime.now(),
-    ).then((pickedDate) {
-      if (pickedDate == null) {
-        return;
-      }
-
-      setState(() {
-        _selectedDate = pickedDate;
-      });
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -59,7 +42,7 @@ class _TransactionFormState extends State<TransactionForm> {
           ),
           child: Column(
             children: [
-               AdaptativeTextField(
+              AdaptativeTextField(
                 label: 'Título',
                 controller: _titleController,
                 onSubmitted: (_) => _submitForm,
@@ -71,34 +54,19 @@ class _TransactionFormState extends State<TransactionForm> {
                     const TextInputType.numberWithOptions(decimal: true),
                 onSubmitted: (_) => _submitForm,
               ),
-              SizedBox(
-                height: 70,
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Text(
-                        _selectedDate == null
-                            ? 'Nenhuma data selecionada!'
-                            : 'Data Selecionada: ${DateFormat('dd/MM/y').format(_selectedDate!)}',
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: _showDatePicker,
-                      child: const Text(
-                        'Selecionar Data',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
+              AdaptativeDatePicker(
+                selectedDate: _selectedDate,
+                onDateChanged: (newDate) {
+                  setState(() {
+                    _selectedDate = newDate;
+                  });
+                },
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
                   AdaptativeButton(
-                    'Nova Transação', 
+                    'Nova Transação',
                     _submitForm,
                   ),
                 ],
